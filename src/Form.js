8a73b00f333a4ee502eea136ed2f2970369682de
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-09-06 13:44:55 +0300

    403 hatası

--- src/Form.js
+++ src/Form.js
@@ -4,22 +4,25 @@ import { useHistory, useLocation } from "react-router-dom";
 import { Input, Label } from "reactstrap";
 import { useState } from "react";
 function MyForm() {
-  const initalData = { name: "deneme", password: "Yapıyorum" };
+  const initalData = { name: "workintech", password: "wecandoit" };
   const history = useHistory();
   const location = useLocation();
   const [loginData, setLoginData] = useState(initalData);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axiosWithAuth().post("login", loginData);
+    axiosWithAuth().post("api/login", loginData);
     console
       .log("LoginData=>", loginData)
 
       .then((res) => {
-        const token = res.data.token;
+        // const token = res.data.token;
         console.log("response", res);
-        localStorage.setItem("token", token);
-        history.push(location.state.referrer);
+        // localStorage.setItem("token", token);
+        // history.push(location.state.referrer);
+      })
+      .catch((err) => {
+        console.loger(err);
       });
   };
   return (
--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ export const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
   console.log("token=>", token);
   return axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:9000",
     headers: token
       ? {
           Authorization: `Bearer ${token}`,
